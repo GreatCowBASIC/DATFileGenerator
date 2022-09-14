c16ad31335da1c2bca59405ff3d9853e8db662a8
Anobium <17933379+Anobium@users.noreply.github.com>
2022-09-14 21:08:45 +0100

    Latest chipfile updates

---
+++ XC8toINC_Converters/PIC10_12_16/16F171xx/16F180x2.bat
@@ -0,0 +1,12 @@
+cd D:\GreatCowBASICGits\DATFileGenerator.git\trunk\XC8toINC_Converters\PIC10_12_16\16F171xx
+call ..\16fprocess 16F18013
+pause
+cd D:\GreatCowBASICGits\DATFileGenerator.git\trunk\XC8toINC_Converters\PIC10_12_16\16F171xx
+call ..\16fprocess 16F18014
+pause
+cd D:\GreatCowBASICGits\DATFileGenerator.git\trunk\XC8toINC_Converters\PIC10_12_16\16F171xx
+call ..\16fprocess 16F18023
+pause
+cd D:\GreatCowBASICGits\DATFileGenerator.git\trunk\XC8toINC_Converters\PIC10_12_16\16F171xx
+call ..\16fprocess 16F18024
+pause
