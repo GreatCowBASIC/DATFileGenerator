bb01f3a3c51fdf825980e76dd491195b08d0333f
Anobium <17933379+Anobium@users.noreply.github.com>
2022-12-09 09:43:15 +0000

    Latest chipfile updates

---
+++ 16F152xx.bat
@@ -0,0 +1,18 @@
+
+getchipdata 16F15213
+getchipdata 16F15214
+getchipdata 16F15223
+getchipdata 16F15224
+getchipdata 16F15225
+getchipdata 16F15243
+getchipdata 16F15244
+getchipdata 16F15245
+getchipdata 16F15254
+getchipdata 16F15256
+getchipdata 16F15274
+getchipdata 16F15275
+getchipdata 16F15276
+
+
+
+
