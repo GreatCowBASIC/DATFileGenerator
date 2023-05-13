95b15173fe59457b5c9b557604f0794a6f4416f6
Anobium <17933379+Anobium@users.noreply.github.com>
2023-05-13 08:02:33 +0100

    Corrected Q83/Q84/Q41 RAM sizes

--- XC8toINC_Converters/PIC18/PIC18Qseries/q41.bat
+++ XC8toINC_Converters/PIC18/PIC18Qseries/q41.bat
@@ -7,8 +7,8 @@ goto %1
 18F_PIC_to_INCv4 PIC18F16Q41  >  D:\DAT\incfiles\OrgFiles\p18F16Q41.inc
 
 gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F04Q41.inc > incfiles\p18F04Q41.inc
-gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F04Q41.inc > incfiles\p18F05Q41.inc
-gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F05Q41.inc > incfiles\p18F06Q41.inc
+gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F05Q41.inc > incfiles\p18F05Q41.inc
+gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F06Q41.inc > incfiles\p18F06Q41.inc
 gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F14Q41.inc > incfiles\p18F14Q41.inc
 gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F15Q41.inc > incfiles\p18F15Q41.inc
 gawk -f preprocessIncFile.awk incfiles\OrgFiles\p18F16Q41.inc > incfiles\p18F16Q41.inc
