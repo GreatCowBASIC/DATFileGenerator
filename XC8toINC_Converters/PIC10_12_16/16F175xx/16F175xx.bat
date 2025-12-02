fea9be4ad2d639c84d7db1ed2304b0ad25ecd224
Anobium <17933379+Anobium@users.noreply.github.com>
2025-12-02 05:52:22 +0000

    Add PIC16F175xx

---
+++ XC8toINC_Converters/PIC10_12_16/16F175xx/16F175xx.bat
@@ -0,0 +1,40 @@
+goto tt
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17524
+pause
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17525
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17526
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17544
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17545
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17546
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17554
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17555
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17556
+
+:tt
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17574
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17575
+
+cd D:\GreatCowBASICGits\DATFileGenerator.git\XC8toINC_Converters\PIC10_12_16\16F175xx
+call ..\16fprocess 16F17576
+
+
